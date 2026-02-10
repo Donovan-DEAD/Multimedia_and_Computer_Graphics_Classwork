08b31d0e70f7f400a217297233580f4daee399e8
Donovan Aguilar <donovan.a.diaz123@gmail.com>
2026-02-10 13:34:11 -0600

    Classwork 02 : Correction on coordinates of the triangule

--- classwork_02/src/main/java/com/github/donovan_dead/Main.java
+++ classwork_02/src/main/java/com/github/donovan_dead/Main.java
@@ -12,16 +12,16 @@ import com.github.donovan_dead.Utils.ColorPoint;
 
 public class Main {
     public static void main(String[] args) {
-        int width = 1920;
-        int height = 1080;
+        int width = 800;
+        int height = 600;
 
         File imgF = new File("Barycentric_Triangule.jpg");
 
         BufferedImage img = new BufferedImage(width,height , BufferedImage.TYPE_INT_RGB);
         
-        ColorPoint c1 = new ColorPoint(new Color(255, 0, 0), width/2, height);
-        ColorPoint c2 = new ColorPoint(new Color(0, 255, 0), 0, 0);
-        ColorPoint c3 = new ColorPoint(new Color(0, 0, 255), width, 0);
+        ColorPoint c1 = new ColorPoint(new Color(255, 0, 0), 0, height);
+        ColorPoint c2 = new ColorPoint(new Color(0, 255, 0), width, height);
+        ColorPoint c3 = new ColorPoint(new Color(0, 0, 255), width/2, 0);
         Triangule t = new Triangule(c1, c2, c3);
 
         for(int i = 0; i< width; i++ ){
